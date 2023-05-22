483be0ad616c209765519273d7a35947e44fbd80
Paweł <pawelnackowski@gmail.com>
2023-05-22 11:57:01 +0200

    deleted unnecessary files

---
+++ src/styled.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const Test = styled.button`
+  color: white;
+  background-color: teal;
+  border: none;
+  border-radius: 10px;
+  padding: 12px;
+  font-size: 20px;
+`
