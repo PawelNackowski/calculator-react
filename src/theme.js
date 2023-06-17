f27f91141e7f0b18878b8b3e6599b25a5a0fef58
Paweł <pawelnackowski@gmail.com>
2023-06-17 20:17:39 +0200

    created theme

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+const colorName = {
+  white: "#FFFFFF",
+  alto: "#DEDEDE",
+  lightGray: "#f2f2f2",
+  silver: "#ABABAB",
+  darkGray: "#141414",
+  mineShaft: " #3D3D3D",
+  black: "#DDDDDD",
+};
+
+export const light = {
+  colors: {
+    background: colorName.alto,
+    body: colorName.darkGray,
+    text: colorName.black,
+    button: colorName.lightGray,
+    buttonHover: colorName.silver,
+  },
+};
+
+export const dark = {
+  colors: {
+    background: colorName.mineShaft,
+    body: colorName.white,
+    text: colorName.alto,
+    button: colorName.lightGray,
+    buttonHover: colorName.silver,
+  },
+};
