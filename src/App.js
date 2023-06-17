2a3df88f36d0b44b535c677cd8fc9a8a4e1256a1
Paweł <pawelnackowski@gmail.com>
2023-06-17 20:14:18 +0200

    added theme provider and globalStyle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { Calculator } from "./Calculator";
+import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "./Calculator/themeSlice";
+import { dark, light } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
+  const darkTheme = useSelector(selectDarkTheme);
   return (
-  <Calculator/>
+    <ThemeProvider theme={darkTheme ? dark : light}>
+      <GlobalStyle />
+      <Calculator />
+    </ThemeProvider>
   );
 }
 
