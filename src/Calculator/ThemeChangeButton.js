fe1af5bbc32ff6f0b488ddc1bf89511d5369d85e
Paweł <pawelnackowski@gmail.com>
2023-06-17 21:45:09 +0200

    changed name changeThemeButton to themeChangeButton

--- src/Calculator/ThemeChangeButton.js
+++ src/Calculator/ThemeChangeButton.js
@@ -2,13 +2,15 @@ import { useDispatch, useSelector } from "react-redux"
 import { ThemeButton } from "./styled"
 import { selectDarkTheme, toggleTheme } from "./themeSlice";
 
-export const ChangeThemeButton = () => {
+export const ThemeChangeButton = () => {
   const dispatch = useDispatch();
   const isDarkTheme = useSelector(selectDarkTheme);
 
   return (
     <div>
-      <ThemeButton onClick={() => dispatch(toggleTheme())}>{isDarkTheme ? "ON" : "OFF"}</ThemeButton>
-      </div>
-  )
-}
+      <ThemeButton onClick={() => dispatch(toggleTheme())}>
+        {isDarkTheme ? "ON" : "OFF"}
+      </ThemeButton>
+    </div>
+  );
+};
