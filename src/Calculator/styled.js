ccdd3ee027c9ef98a027040c9c53ef7438585406
Paweł <pawelnackowski@gmail.com>
2023-06-18 16:51:41 +0200

    changed styled

--- src/Calculator/styled.js
+++ src/Calculator/styled.js
@@ -1,31 +1,34 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.form`
+  font-family: "Orbitron";
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   border: 1px solid black;
   border-radius: 10px;
   box-shadow: 4px 4px 8px 2px black;
-  height: 500px;
+  min-height: 400px;
+  width: 300px;
   padding: 20px;
   gap: 10px;
-  background-color: ${({theme}) => theme.colors.body};
-  transition: background-color 1s ;
+  background-color: ${({ theme }) => theme.colors.body};
+  transition: background-color 1s;
 `;
 
 export const Input = styled.input`
+  font-family: "Orbitron";
   display: flex;
   align-content: stretch;
   text-align: right;
   padding: 10px;
   height: 50px;
-  margin-top: 16px;
+  margin-bottom: 6px;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 26px;
   letter-spacing: 2px;
-  box-shadow: inset -2px -2px 2px 1px gray;
-  border: 1px solid ${({theme}) => theme.colors.background};
+  box-shadow: inset -2px -2px 2px 3px gray;
+  border: 1px solid ${({ theme }) => theme.colors.gray};
   border-radius: 5px;
 
   &::placeholder {
@@ -38,26 +41,45 @@ export const Input = styled.input`
 
 export const Numbers = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr );
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 8px;
 `;
 
 export const Button = styled.button`
-  font-size: 30px;
-  width: 70px;
-  height: 56px;
+  font-family: "Orbitron";
+  font-size: 28px;
+  font-weight: 600;
+  min-width: ${(props) => (props.clear ? "128px" : "60px")};
+  height: 50px;
   margin: auto;
-  box-shadow: inset -2px -2px 2px 1px gray;
+  box-shadow: inset -3px -3px 2px 3px gray;
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.background};
+  }
+  &:active {
+    background-color: ${({ theme }) => theme.colors.text};
+  }
 `;
 
 export const ThemeButton = styled.button`
-  position: fixed;
-  width:  60px;
+  font-family: "Orbitron";
+  font-weight: bold;
+  background-color: ${({ theme }) => theme.modeButton.background};
+  color: ${({ theme }) => theme.modeButton.text};
+  width: 60px;
   height: 30px;
-  margin: auto;
-  font-size: 10px;
-`
+  margin-left: 10px;
+  font-size: 15px;
+  border-radius: 5px;
+  box-shadow: inset -1px -1px 2px 3px gray;
+`;
 
-export const ClearPosition = styled.div`
-
-`
+export const ButtonModePosition = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.text};
+  `;
